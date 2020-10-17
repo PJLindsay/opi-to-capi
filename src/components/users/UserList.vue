994b3e08c3b4d014dde22110b1dcdc5db69dc55a
PJ Lindsay <pjlindsay@gmail.com>
2020-10-17 16:23:56 -0600

    Make reusable sort hook

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -26,9 +26,10 @@
 </template>
 
 <script>
-import { computed, ref, toRefs } from 'vue';
+import { toRefs } from 'vue';
 import UserItem from './UserItem.vue';
 import useSearch from '../../hooks/search.js';
+import useSort from '../../hooks/sort.js';
 
 export default {
   components: {
@@ -47,27 +48,11 @@ export default {
     );
 
     /*  ----------------------  SORT LOGIC ---------------------- */
-    const sorting = ref(null);
-    const displayedUsers = computed(function() {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    /*  ---------------  moved to /hooks/sort.js ---------------- */
+    const { sorting, displayedUsers, sort } = useSort(
+      availableItems,
+      'fullName'
+    )
 
     /*  ----------------------  Expose methods/computed/variables to template ---------------------- */
     return {
